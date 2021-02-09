60c1c8174864f1446b28228b2adbca6da785956b
br34th3r <joshuaboddy1@gmail.com>
2021-02-09 03:00:57 +0000

    Update to SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ export default class NotFoundPage extends Component {
   render() {
     return (
       <div>
-        <SEO title="404: Not found" />
+        <SEO title="404 | Josh Boddy" />
         <h1>404: Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </div>
